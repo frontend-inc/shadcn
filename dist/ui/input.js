d654daf45f588d37a1dd6d394aec8044c4b8b64a
Rami Bitar <rbitar@gmail.com>
2024-10-29 18:26:25 -0400

    deploy

--- dist/ui/input.js
+++ dist/ui/input.js
@@ -50,7 +50,7 @@ var React = __importStar(require("react"));
 var utils_1 = require("../lib/utils");
 var Input = React.forwardRef(function (_a, ref) {
     var className = _a.className, type = _a.type, props = __rest(_a, ["className", "type"]);
-    return (React.createElement("input", __assign({ type: type, className: (0, utils_1.cn)("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50", className), ref: ref }, props)));
+    return (React.createElement("input", __assign({ type: type, className: (0, utils_1.cn)("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-foreground", className), ref: ref }, props)));
 });
 exports.Input = Input;
 Input.displayName = "Input";
