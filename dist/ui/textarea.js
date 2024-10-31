7c904a7f45fc286c01272ccc9b60deaeee9a0bbf
Rami Bitar <rbitar@gmail.com>
2024-10-30 20:30:33 -0400

    deploy

--- dist/ui/textarea.js
+++ dist/ui/textarea.js
@@ -50,7 +50,7 @@ var React = __importStar(require("react"));
 var utils_1 = require("../lib/utils");
 var Textarea = React.forwardRef(function (_a, ref) {
     var className = _a.className, props = __rest(_a, ["className"]);
-    return (React.createElement("textarea", __assign({ className: (0, utils_1.cn)('flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', className), ref: ref }, props)));
+    return (React.createElement("textarea", __assign({ className: (0, utils_1.cn)('flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-foreground', className), ref: ref }, props)));
 });
 exports.Textarea = Textarea;
 Textarea.displayName = 'Textarea';
