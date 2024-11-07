dee1df03cdb0df1ee19111d7db4f16d563f28fb9
Rami Bitar <rbitar@gmail.com>
2024-11-07 12:50:16 -0500

    deploy

--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:ring-1 focus:ring-ring focus:ring-offset-3 disabled:cursor-not-allowed disabled:opacity-50 text-foreground",
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus:ring-1 focus:ring-ring focus:ring-offset-3 disabled:cursor-not-allowed disabled:opacity-50 text-foreground",
           className
         )}
         ref={ref}
--- src/ui/textarea.tsx
+++ src/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
 		return (
 			<textarea
 				className={cn(
-					'flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus:ring-1 focus:ring-ring focus:ring-offset-3 disabled:cursor-not-allowed disabled:opacity-50 text-foreground',
+					'flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus:ring-1 focus:ring-ring focus:ring-offset-3 disabled:cursor-not-allowed disabled:opacity-50 text-foreground',
 					className
 				)}
 				ref={ref}
