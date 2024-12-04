ac33f77dbb69f87ca01c4d0df3f1adfcf5ad1222
Rami Bitar <rbitar@gmail.com>
2024-12-04 14:10:34 -0500

    deploy

--- dist/ui/carousel.js
+++ dist/ui/carousel.js
@@ -140,7 +140,7 @@ CarouselItem.displayName = "CarouselItem";
 var CarouselPrevious = React.forwardRef(function (_a, ref) {
     var className = _a.className, _b = _a.variant, variant = _b === void 0 ? "outline" : _b, _c = _a.size, size = _c === void 0 ? "icon" : _c, props = __rest(_a, ["className", "variant", "size"]);
     var _d = useCarousel(), orientation = _d.orientation, scrollPrev = _d.scrollPrev, canScrollPrev = _d.canScrollPrev;
-    return (React.createElement(button_1.Button, __assign({ ref: ref, variant: variant, size: size, className: (0, utils_1.cn)("absolute h-8 w-8 rounded-full", orientation === "horizontal"
+    return (React.createElement(button_1.Button, __assign({ ref: ref, variant: variant, size: size, className: (0, utils_1.cn)("absolute h-12 w-12 rounded-full", orientation === "horizontal"
             ? "-left-12 top-1/2 -translate-y-1/2"
             : "-top-12 left-1/2 -translate-x-1/2 rotate-90", className), disabled: !canScrollPrev, onClick: scrollPrev }, props),
         React.createElement(lucide_react_1.ChevronLeft, { className: "h-8 w-8" }),
@@ -151,7 +151,7 @@ CarouselPrevious.displayName = "CarouselPrevious";
 var CarouselNext = React.forwardRef(function (_a, ref) {
     var className = _a.className, _b = _a.variant, variant = _b === void 0 ? "outline" : _b, _c = _a.size, size = _c === void 0 ? "icon" : _c, props = __rest(_a, ["className", "variant", "size"]);
     var _d = useCarousel(), orientation = _d.orientation, scrollNext = _d.scrollNext, canScrollNext = _d.canScrollNext;
-    return (React.createElement(button_1.Button, __assign({ ref: ref, variant: variant, size: size, className: (0, utils_1.cn)("absolute h-8 w-8 rounded-full", orientation === "horizontal"
+    return (React.createElement(button_1.Button, __assign({ ref: ref, variant: variant, size: size, className: (0, utils_1.cn)("absolute h-12 w-12 rounded-full", orientation === "horizontal"
             ? "-right-12 top-1/2 -translate-y-1/2"
             : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90", className), disabled: !canScrollNext, onClick: scrollNext }, props),
         React.createElement(lucide_react_1.ChevronRight, { className: "h-8 w-8" }),
