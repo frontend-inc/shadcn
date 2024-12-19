059f3b71b8d58a2ef7a8ae00c622718077349aad
Rami Bitar <rbitar@gmail.com>
2024-12-19 14:35:53 -0500

    deploy

--- dist/ui/sheet.js
+++ dist/ui/sheet.js
@@ -86,10 +86,10 @@ var SheetContent = React.forwardRef(function (_a, ref) {
         React.createElement(SheetPrimitive.Content, __assign({ ref: ref, className: 
             //@ts-ignore
             (0, utils_1.cn)(sheetVariants({ side: side }), className) }, props),
+            children,
             React.createElement(SheetPrimitive.Close, { className: "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary" },
                 React.createElement(react_icons_1.Cross2Icon, { className: "h-5 w-5 text-foreground" }),
-                React.createElement("span", { className: "sr-only" }, "Close")),
-            children)));
+                React.createElement("span", { className: "sr-only" }, "Close")))));
 });
 exports.SheetContent = SheetContent;
 SheetContent.displayName = SheetPrimitive.Content.displayName;
